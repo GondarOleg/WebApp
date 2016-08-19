345bdaed5e58ee9e6c27b84ec9e84e5c6c3290a9
Oleg Gondar <Oleg_Gondar@epam.com>
2016-08-19 17:58:08 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by O.Gondar on 19.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("WebApp!!!!");
+    }
+
+    public static boolean alwaysTrue(){
+        return true;
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by O.Gondar on 19.08.2016.
+ */
+public class TestMain {
+
+    @Test
+    public void checkAlwaysTrueFunction(){
+        assertTrue(Main.alwaysTrue());
+    }
+
+}
